2ab429d99544ea41262f08dd83a031b50aed3c47
Vinaypunani <114213456+Vinaypunani@users.noreply.github.com>
2024-05-26 09:02:53 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose.connect("mongodb+srv://vinaypunani:C9218730@cluster0.8zbnsyo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+    console.log("Connected");
+})
+.catch((err)=>{
+    console.log('Error : ', err)
+})
 
 
 const userSchema =new mongoose.Schema({
